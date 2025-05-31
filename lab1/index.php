e477b536e7316d82789d1c9e68b92e7ed95f98fd
dhjhjnbukg <maxiusa2005@gmail.com>
2025-05-31 17:49:39 +0300

    Выполнена 1 лабораторная

--- lab1/index.php
+++ lab1/index.php
@@ -1,3 +1,18 @@
 <?php
+/* echo "Привет, мир!"; */
 
-echo "Привет, мир!";
+/* echo "Hello, World with echo!";
+   print "Hello, World with print!"; */
+
+   /* $days = 288;
+$message = "Все возвращаются на работу!";
+
+//вывод с использованием конкатенации:
+echo $days . " дней. " . $message . "<br>";
+
+//вывод с использованием двойных кавычек:
+echo "$days дней. $message<br>";
+
+//использование HTML-тега для перевода строки:
+echo $days . " дней.<br>" . $message; */
+?>
